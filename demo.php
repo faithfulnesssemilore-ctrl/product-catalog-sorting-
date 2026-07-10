ccd7dc1491b26b02cb839a0b56df84be8a1e21b6
Semilore <semilore@example.com>
2026-07-10 15:15:20 +0100

    feat: Run example script

--- demo.php
+++ demo.php
@@ -14,7 +14,7 @@ $products = [
     ['id' => 1, 'name' => 'Alabaster Table', 'price' => 12.99, 'created_at' => '2019-01-04', 'sales_count' => 32, 'views_count' => 730],
     ['id' => 2, 'name' => 'Zebra Table', 'price' => 44.49, 'created_at' => '2012-01-04', 'sales_count' => 301, 'views_count' => 3279],
     ['id' => 3, 'name' => 'Coffee Table', 'price' => 10.00, 'created_at' => '2014-05-28', 'sales_count' => 1048, 'views_count' => 20123],
-]/;//define the product array with sample data 
+];//define the product array with sample data 
 
 $catalog = new Catalog($products); //call the instance of the Catalog class and pass the product array to it.
 
